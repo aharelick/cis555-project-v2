90cf4969be068adfbbb8dd39c1d896869eb407bc
Alex Harelick <alexharelick@gmail.com>
2015-05-02 17:23:05 -0400

    changed site to include body and children

--- src/edu/upenn/cis555/crawler/storage/Site.java
+++ src/edu/upenn/cis555/crawler/storage/Site.java
@@ -1,5 +1,7 @@
 package edu.upenn.cis555.crawler.storage;
 
+import java.util.LinkedList;
+
 import com.sleepycat.persist.model.Entity;
 import com.sleepycat.persist.model.PrimaryKey;
 
@@ -9,6 +11,8 @@ public class Site {
 	@PrimaryKey
 	private String url;
 	private String contentType;
+	private String body;
+	private LinkedList<String> children;
 	private long nextRequestTime;
 
 	
@@ -19,6 +23,7 @@ public class Site {
 	public Site(String url, long nextRequestTime) {
 		this.url = url;
 		this.nextRequestTime = nextRequestTime;
+		children = new LinkedList<String>();
 	}
 	
 	public String getSite() {
@@ -28,9 +33,28 @@ public class Site {
 	public void setContentType(String contentType) {
 		this.contentType = contentType;
 	}
+	
 	public String getContentType() {
 		return contentType;
 	}
+	
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+	public void setChildren(LinkedList<String> children) {
+		this.children = children;
+	}
+	
+	public String getBody() {
+		return body;
+	}
+	
+	public LinkedList<String> getChildren() {
+		return children;
+	}
+	
+	
 	
 	/**
 	 * If return value is negative, the time to crawl has already
