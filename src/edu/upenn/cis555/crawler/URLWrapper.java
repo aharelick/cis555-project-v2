527befc2f07eea1b3e6632b4734544c068135720
Alex Harelick <alexharelick@gmail.com>
2015-05-02 14:22:10 -0400

    batch pull working

--- src/edu/upenn/cis555/crawler/URLWrapper.java
+++ src/edu/upenn/cis555/crawler/URLWrapper.java
@@ -1,5 +1,62 @@
 package edu.upenn.cis555.crawler;
 
-public class URLWrapper {
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
+public class URLWrapper {
+	URL url = null;
+	boolean goodURL = true;
+
+	/**
+	 * Wrapper method for the URL. Parses the given string 
+	 * and makes it a full URL with protocol and hostname.
+	 * @param link
+	 * @param protocol
+	 * @param hostname
+	 * @param path
+	 */
+	public URLWrapper(String link, String protocol, String hostname, String path) {
+		try {
+			URI uri = new URI(link);
+			
+			if (link.startsWith("//")) {
+				link = protocol + ":" + link; 
+				url = new URL(link);
+				return;
+			}
+			if ((path != null) && (path.trim().equals(""))) {
+				path = "/";
+			}
+			if ((path != null) && (path.substring(path.lastIndexOf('/'))).contains(".")) {
+				path = path.substring(0, path.lastIndexOf('/')).trim();
+			}
+			if ((path != null) && !path.endsWith("/")) {
+				path += "/";
+			}
+			if (!uri.isAbsolute() && (uri.getHost() == null) && (hostname != null)) {
+				if (!link.startsWith("/")) {
+					link = hostname + path  + link;
+				} else {
+					link = hostname + link;
+				}
+			}
+
+			if (!uri.isAbsolute() && (uri.getScheme() == null) && (protocol != null)) {
+				link = protocol + "://" + link;
+			}
+			url = new URL(link);
+		} catch (MalformedURLException | URISyntaxException e) {
+			goodURL = false;
+		}
+	}
+	
+	public URL getURL() {
+		return url;
+	}
+	
+	public boolean isGoodURL() {
+		return goodURL;
+	}
 }
--- src/edu/upenn/cis555/crawler/storage/DBWrapper.java
+++ src/edu/upenn/cis555/crawler/storage/DBWrapper.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 
 import com.sleepycat.je.Environment;
 import com.sleepycat.je.EnvironmentConfig;
+import com.sleepycat.je.LockMode;
+import com.sleepycat.persist.EntityCursor;
 import com.sleepycat.persist.EntityStore;
 import com.sleepycat.persist.PrimaryIndex;
 import com.sleepycat.persist.StoreConfig;
@@ -15,6 +17,7 @@ public class DBWrapper {
 	private static EntityStore store;
 	
 	private static PrimaryIndex<String, Site> headQueue;
+	private static PrimaryIndex<String, Site> getQueue;
 	
 	/**
 	 * Create the DB if it doesn't exist and open it if it does exist.
@@ -41,14 +44,29 @@ public class DBWrapper {
         System.out.println(dir.getAbsolutePath());
         store = new EntityStore(myEnv, "EntityStore", storeConfig);
         headQueue = store.getPrimaryIndex(String.class, Site.class);
+        getQueue = store.getPrimaryIndex(String.class, Site.class);
         DatabaseShutdownHook hook = new DatabaseShutdownHook(myEnv, store);
         Runtime.getRuntime().addShutdownHook(hook);
         System.out.println("Database Started");
 	}
 	
 	public static LinkedList<Site> batchPullFromHead(int count) {
-		//return headQueue.get(term);
-		return null;
+		EntityCursor<Site> cursor = headQueue.entities();
+		LinkedList<Site> sites = new LinkedList<Site>();
+		try {
+			Site entity;
+			while ((entity = cursor.next(LockMode.RMW)) != null) {
+				sites.add(entity);
+				System.out.println(entity.getSite());
+				cursor.delete();
+				if (sites.size() == count) {
+					break;
+				}
+			}
+		} finally {
+			cursor.close();
+		}	
+		return sites;
 	}
 	
 	public static void putToHeadQueue(Site site) {
@@ -57,6 +75,32 @@ public class DBWrapper {
 	
 	public static void deleteFromHeadQueue(String url) {
 		headQueue.delete(url);
+	}
+	
+	public static LinkedList<Site> batchPullFromGet(int count) {
+		EntityCursor<Site> cursor = getQueue.entities();
+		LinkedList<Site> sites = new LinkedList<Site>();
+		try {
+			Site entity;
+			while ((entity = cursor.next(LockMode.RMW)) != null) {
+				sites.add(entity);
+				cursor.delete();
+				if (sites.size() == count) {
+					break;
+				}
+			}
+		} finally {
+			cursor.close();
+		}	
+		return sites;
+	}
+	
+	public static void putToGetQueue(Site site) {
+		getQueue.put(site);
+	}
+	
+	public static void deleteFromGetQueue(String url) {
+		getQueue.delete(url);
 	}
 
 	public static void sync() {
